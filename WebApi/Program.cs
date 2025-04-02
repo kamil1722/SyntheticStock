400a71c839fde041e8819d6b7e26c7e7d9d9154b
Kamil <kamilaliev1722@gmail.com>
2025-04-02 19:20:27 +0300

    fix

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -24,8 +24,8 @@ builder.Services.AddTransient<IBinanceRestClient, BinanceRestClient>(provider =>
     var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
     var logger = loggerFactory.CreateLogger<BinanceRestClient>();
 
-    var apiKey = configuration["BINANCE_APISECRET"];
-    var apiSecret = configuration["BINANCE_APISECRET"];
+    var apiKey = configuration["Binance:ApiKey"];
+    var apiSecret = configuration["Binance:ApiSecret"];
 
     if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(apiSecret))
     {
